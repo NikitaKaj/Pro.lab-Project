6561721153ad0c8946ae2587854d28f29cc0d810
NikitaKaj <nikitakajurins1@gmail.com>
2025-10-23 16:02:59 +0300

    Исправлена ошибка с скролами справа и снизу

--- frontend/pro-lab-frontend/src/App.vue
+++ frontend/pro-lab-frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex h-screen w-screen">
+  <div class="flex min-h-screen">
     <Sidebar />
-    <main class="flex-1 overflow-y-auto bg-gray-100">
+    <main class="flex-1 min-w-0 overflow-y-auto bg-gray-100">
       <RouterView />
     </main>
   </div>
