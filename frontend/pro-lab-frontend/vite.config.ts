78f2a39d3b59ce73389b73c86ae10c31554e8e60
brajgor <artemij.burovs@gmail.com>
2026-01-06 23:40:41 +0200

    deploy

--- frontend/pro-lab-frontend/vite.config.ts
+++ frontend/pro-lab-frontend/vite.config.ts
@@ -8,10 +8,15 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwind()],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@components': fileURLToPath(new URL('./src/components', import.meta.url))
     },
   },
+  build: {
+    outDir: '../../backend/src/Api.ProLab/nuxtApp',
+    emptyOutDir: true
+  }
 })
